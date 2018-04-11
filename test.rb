f0a1cafe6ecdc8e3de2911c32b7306931efe7c8b
Maxim Golikov <blikylia@gmail.com>
2018-04-11 18:48:48 +0300

    Rename file with test program

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require './stemmer.rb'
+
+abort 'No command line arguments' if ARGV.length.zero?
+# abort 'File does not exists!' if File.exist?(ARGV[0])
+$file_path = ARGV[0]
+
+# Test Porter's stemmer
+class TestStemmer < Test::Unit::TestCase
+  def test_simple
+    stemmer = Stemmer.new
+    file = File.open $file_path
+    while (line = file.gets)
+      l, r = line.encode('UTF-8').chomp.split
+      assert_equal(r, stemmer.stem(l))
+    end
+  end
+end
