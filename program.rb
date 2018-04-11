ca714430880b9744543066aa45c60614bdb8882d
Maxim Golikov <blikylia@gmail.com>
2018-04-11 22:24:55 +0300

    Start development algorithm of fuzzy search

--- program.rb
+++ program.rb
@@ -1,18 +1,64 @@
-require 'test/unit'
 require './stemmer.rb'
 
-abort 'No command line arguments' if ARGV.length.zero?
-# abort 'File does not exists!' if File.exist?(ARGV[0])
-$file_path = ARGV[0]
-
-# Test Porter's stemmer
-class TestStemmer < Test::Unit::TestCase
-  def test_simple
-    stemmer = Stemmer.new
-    file = File.open $file_path
+def initialize_stemmer_dictionary(file_path)
+  dictionary = {}
+  File.open(file_path, 'r') do |file|
     while (line = file.gets)
-      l, r = line.encode('UTF-8').chomp.split
-      assert_equal(r, stemmer.stem(l))
+      word, word_base = line.encode('UTF-8').chomp.split
+      dictionary[word_base] << word if dictionary[word_base]
+      dictionary[word_base] = [word] unless dictionary[word_base]
     end
+  end
+  dictionary
+end
+
+def levenshtein(word1, word2)
+  m, n = word1.length, word2.length
+  distance = []
+  (m + 1).times { distance << [0] * (n + 1) }
+  distance[0] = distance.first.each_with_index.map { |_, i| i }
+  distance.each_with_index { |e, i| e[0] = i }
+  (1..(distance.length - 1)).each do |i|
+    (1..(distance[i].length - 1)).each do |j|
+      distance[i][j] = if word1[i - 1] == word2[j - 1]
+                         distance[i - 1][j - 1]
+                       else
+                         a = [distance[i - 1][j - 1], distance[i][j - 1], distance[i - 1][j]]
+                         # p a
+                         a.min + 1
+                       end
+    end
+  end
+  distance.last.last
+end
+
+def n_gramm(word, length)
+  (0..word.length - length).map { |i| puts word[i, length] }
+end
+
+abort 'No command line arguments' if ARGV.length.zero?
+
+puts 'Initialazing dictionary for stemming. Wait please...'
+stemmer_dictionary = initialize_stemmer_dictionary(ARGV[0])
+puts 'Dictionary initialazed!'
+
+stemmer = Stemmer.new
+
+loop do
+  puts 'Enter 1 to find words with the same base part'
+  puts 'Enter 2 to find all possible word\'s corrections'
+  print 'Your answer: '
+  answer = $stdin.gets.chomp.to_i
+  case answer
+  when 0
+    break
+  when 1
+    word = $stdin.gets.chomp
+    same_words = stemmer_dictionary[stemmer.stem(word)].join(',')
+    puts "All words with the same base: #{same_words}\n\n"
+  when 2
+    puts '123123'
+  else
+    puts "Wrong answer\n\n"
   end
 end
